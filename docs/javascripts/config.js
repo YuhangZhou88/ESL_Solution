80da66bda8781749131a92c11fd814ce15e1889f
YuhangZhou88 <zhouyhnk@gmail.com>
2021-04-04 15:49:37 -0400

    add 3.13 and mathjax macros

--- docs/javascripts/config.js
+++ docs/javascripts/config.js
@@ -4,7 +4,14 @@ window.MathJax = {
       displayMath: [["\\[", "\\]"]],
       processEscapes: true,
       processEnvironments: true,
-      tags: 'ams'
+      tags: 'ams',
+      macros: {
+        bb: ["{\\textbf #1}", 1],
+        bX: "\\textbf{X}",
+        bx: "\\textbf{x}",
+        bY: "\\textbf{Y}",
+        by: "\\textbf{y}"
+      }
     },
     options: {
       ignoreHtmlClass: ".*|",
