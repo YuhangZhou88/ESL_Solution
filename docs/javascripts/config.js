29ca5aeaa4cffbda484e1b43f2944178719591ec
YuhangZhou88 <zhouyhnk@gmail.com>
2021-04-05 22:06:28 -0400

    finish chapter 6

--- docs/javascripts/config.js
+++ docs/javascripts/config.js
@@ -17,6 +17,7 @@ window.MathJax = {
         ra: "\\rightarrow",
         bI: "\\textbf{I}",
         bB: "\\textbf{B}",
+        bW: "\\textbf{W}",
         rl: "\\rangle",
         bm: ["\\boldsymbol #1", 1]
       }
